f5ffd1ae30d2d85ce66b3085156329d8e5fc1674
Ganesh Sri Prasad K B <113231602+ganeshsriprasad@users.noreply.github.com>
2024-11-14 10:53:29 +0530

    Update urls.py

--- blogverse/urls.py
+++ blogverse/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('eha/donttrytohackadminpagethis-is-the-universal-admin-panel-lol/', admin.site.urls ),
 ]
 
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
